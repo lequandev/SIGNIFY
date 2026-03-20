bc3dbf426d18cee12cd54c68e5faaf967e785c1b
trantheluong1510 <de180077trantheluong@gmail.com>
2026-03-20 22:50:01 +0700

    feat/login&register: add

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,17 @@ import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 import { store } from './redux/store';
 import { router } from './router';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import './global.css';
+
+const GOOGLE_CLIENT_ID = "803514572954-ou80dm60ceo6uiaojtjb4aeja6tsaqgb.apps.googleusercontent.com";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,9 +1,24 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import VerifyEmail from './pages/VerifyEmail';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/verify-email/:token',
+    element: <VerifyEmail />,
   },
 ]);
